2ffe5cda7ee4576ebe85c656c3e0f24b1136794f
EC2 Default User <ec2-user@ip-172-31-29-56.ec2.internal>
2022-08-20 13:13:18 +0000

    Fix　protected in sessions_controller

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  #before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,10 +19,29 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+    def customer_state
+  　   @customer = Customer.find_by(email: params[:customer][:email])
+      if @customer
+        if @customer.valid_password?(params[:customer][:password]) && !@customer.is_active
+          redirect_to new_user_session_path
+        # else
+        #   redirect_to root_path
+        end
+      end
 
+      # # ture && true
+      # if @customer && @customer.valid_password?(params[:customer][:password]) && !@customer.is_active
+      #   redirect_to new_user_session_path
+      # end
+      # # true && flase
+      # if @customer && !(@customer.valid_password?(params[:customer][:password]) && !@customer.is_active)
+      #   redirect_to new_user_session_path
+      # end
+
+    end
+end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-end
+
