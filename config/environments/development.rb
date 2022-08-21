57279350b640137e135de5405e8a6570cb1e0d1b
EC2 Default User <ec2-user@ip-172-31-29-56.ec2.internal>
2022-08-21 15:00:40 +0000

    Add view in header

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "a1dd3dcac102476b9758dcb730c0d38b.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
 end
