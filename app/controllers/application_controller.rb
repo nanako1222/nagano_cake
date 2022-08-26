9c0a4287a112267f30e1693fe4d2c1ee04bdc12c
EC2 Default User <ec2-user@ip-172-31-29-56.ec2.internal>
2022-08-26 13:46:32 +0000

    Create page new of public/registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+  end
+
   def after_sign_in_path_for(resource)
     case resource
     when Admin
@@ -10,5 +16,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     admin_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :adress, :telephone_number])
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,16 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
+
+  before_create :create_name
+  def create_name
+    self.name = "#{first_name} #{last_name}"
+  end
+
+  before_create :create_name_kana
+  def create_name_kana
+    self.name_kana = "#{first_name_kana} #{last_name_kana}"
+  end
 end
