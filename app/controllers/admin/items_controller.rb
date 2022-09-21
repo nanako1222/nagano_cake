e80811d7176c41f0413c32c66f5efb5aeebfd8b7
EC2 Default User <ec2-user@ip-172-31-29-56.ec2.internal>
2022-09-21 12:37:48 +0000

    Change link of admin/item/new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,7 +13,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_items_path, notice: 'Product was successfully created'
+      redirect_to admin_item_path(@item.id), notice: 'Product was successfully created'
     else
       render :new
     end
